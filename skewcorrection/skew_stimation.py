5ca0dd77798491b3b05b464cef26b146729ea30a
Fatemeh-Khorshidi <ftmh.khorshidi@gmail.com>
2023-11-04 04:28:10 +0330

    rename files

--- skewcorrection/skew_stimation.py
+++ skewcorrection/skew_stimation.py
@@ -4,6 +4,19 @@ from skimage.transform import (hough_line, hough_line_peaks, probabilistic_hough
 
 
 def Skewed_angle(x1, y1, x2, y2):
+    """
+    Calculate the angle (in degrees) between two points (x1, y1) and (x2, y2) relative to the positive x-axis.
+    Returns 0 if a ZeroDivisionError occurs.
+
+    Parameters:
+    - x1 (float): x-coordinate of the first point.
+    - y1 (float): y-coordinate of the first point.
+    - x2 (float): x-coordinate of the second point.
+    - y2 (float): y-coordinate of the second point.
+
+    Returns:
+    - angle (float): The angle between the two points in degrees.
+    """
     try:
         y = (y2 - y1)  # numerator of arctan
         x = (x2 - x1)  # denominator of arctan
@@ -15,6 +28,15 @@ def Skewed_angle(x1, y1, x2, y2):
 
 
 def estimate_skewness(binary_image):
+    """
+    Estimate the skewness angle of a binary image using the Hough line transform.
+
+    Parameters:
+    - binary_image (numpy.ndarray): A binary image (e.g., after edge detection).
+
+    Returns:
+    - skewness_angle (float): The estimated skewness angle in degrees.
+    """
     # Classic straight-line Hough transform
     h, theta, d = hough_line(binary_image)
 
@@ -40,6 +62,16 @@ def estimate_skewness(binary_image):
 
 # de-skew the image
 def de_skew(img, angle):
+    """
+    De-skew an image by rotating it by a specified angle.
+
+    Parameters:
+    - img (numpy.ndarray): The input image to be de-skewed.
+    - angle (float): The angle (in degrees) by which to rotate the image for de-skewing.
+
+    Returns:
+    - de_skewed_img (numpy.ndarray): The de-skewed image.
+    """
     rows, cols, _ = img.shape
     # print ("Angle : ", angle)
 
